e396a97ada84e2a1f4589f5a8e580395d6f8505c
iamdeve <wajdanaeli@gmail.com>
2020-12-21 21:53:32 +0500

    client updated

--- routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/auth');
-
-router.post('/signup', authController.signupWithCognito);
-router.post('/login', authController.loginWithCognito);
-
-module.exports = router;
--- server.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-const app = require('./app');
-const hostname = '127.0.0.1';
-const port = process.env.port || 3001;
-const server = http.createServer(app);
-server.listen(port,'0.0.0.0', () =>
-console.log(`Server Started At PORT: ${port} {Finparo-App-Backend}`));
