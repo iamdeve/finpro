6a12a54d68906cedc14e6e740c0ed3a1bf94cbca
iamdeve <wajdanaeli@gmail.com>
2021-01-27 15:00:51 +0500

    url changes

--- src/context/axios.js
+++ src/context/axios.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 axios.defaults.headers.common['authorization'] = `${localStorage.getItem('finProtoken')}`;
 const instance = axios.create({
 	// baseURL: 'http://localhost:3000',
-	baseURL: 'https://emmb3fy0r8.execute-api.us-east-1.amazonaws.com/dev',
+	baseURL: 'https://gqy6zluplk.execute-api.us-east-1.amazonaws.com',
 	// baseURL: 'https://finpro-api-test.herokuapp.com/api/',
 });
 
 // export const BASE_URL = 'https://finpro-api-test.herokuapp.com/';
 // export const BASE_URL = 'http://localhost:3001/';
-export const BASE_URL = 'https://emmb3fy0r8.execute-api.us-east-1.amazonaws.com/dev';
+export const BASE_URL = 'https://gqy6zluplk.execute-api.us-east-1.amazonaws.com';
 export default instance;
 
 /* test credentials
